5a9bafa400f7070be1763fc5ab2ab8eb1becee81
Porrith Suong <porrith.suong@gmail.com>
2022-05-23 21:06:36 -0400

    Added stb_vorbis implementation.

--- src/miniaudio.c
+++ src/miniaudio.c
@@ -1,2 +1,8 @@
+#define STB_VORBIS_HEADER_ONLY
+#include "../miniaudio/extras/stb_vorbis.c"
+
 #define MINIAUDIO_IMPLEMENTATION
 #include "../miniaudio/miniaudio.h"
+
+#undef STB_VORBIS_HEADER_ONLY
+#include "../miniaudio/extras/stb_vorbis.c"
